40f3a0a4acfb3c5742486f04cd7296140296e5f3
Naratip <naratip.18257@gmail.com>
2023-06-11 13:46:12 +0700

    create function createGrid

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+function createGrid( size = 16){
+    const container = document.querySelector("#container");
+    const grid = document.createElement("div");
+    grid.style.cssText = `padding:${size};margin:0;border:0;`;
+    grid.classList.add("paper");
+}
