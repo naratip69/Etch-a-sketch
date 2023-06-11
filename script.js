6b38ca2b87b1c5ddabc60d2a2ac1df5472f6b240
Naratip <naratip.18257@gmail.com>
2023-06-11 15:26:45 +0700

    create changeColor function and addEventlisener to grid

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
+function changeColor(e){
+    this.style.cssText += "background-color:black";
+}
+
 function createGrid( size = 16){
     const container = document.querySelector("#container");
     for(let i=0;i<size;i++){
@@ -6,10 +10,12 @@ function createGrid( size = 16){
         line.classList.add("line");
         for( let j=0;j<size;j++){
             const grid = document.createElement("div");
-            grid.style.cssText = `padding:2px;margin:0;border:0;`;
+            grid.style.cssText = `padding:4px;margin:0;border:0;`;
             grid.classList.add("paper");
+            grid.addEventListener("mouseenter",changeColor);
             line.appendChild(grid);
         }
         container.appendChild(line);
     }
 }
+
