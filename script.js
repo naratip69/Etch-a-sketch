d756b2ceb2a2cddf031458092b72efb2a11897b3
Naratip <naratip.18257@gmail.com>
2023-06-11 13:51:25 +0700

    add px to padding of grid

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function createGrid( size = 16){
     const container = document.querySelector("#container");
     const grid = document.createElement("div");
-    grid.style.cssText = `padding:${size};margin:0;border:0;`;
+    grid.style.cssText = `padding:${size}px;margin:0;border:0;`;
     grid.classList.add("paper");
     container.appendChild(grid);
 }
