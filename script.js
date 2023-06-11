c0f23c59fbc45679a2748690736e2dad410d9db7
Naratip <naratip.18257@gmail.com>
2023-06-11 14:13:27 +0700

    make createGrid to create grid

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
 function createGrid( size = 16){
     const container = document.querySelector("#container");
-    const grid = document.createElement("div");
-    grid.style.cssText = `padding:${size}px;margin:0;border:0;`;
-    grid.classList.add("paper");
-    container.appendChild(grid);
+    for(let i=0;i<size;i++){
+        const line = document.createElement("div");
+        line.classList.add("line");
+        for( let j=0;j<size;j++){
+            const grid = document.createElement("div");
+            grid.style.cssText = `padding:2px;margin:0;border:0;`;
+            grid.classList.add("paper");
+            line.appendChild(grid);
+        }
+        container.appendChild(line);
+    }
 }
