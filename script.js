e5bc41c5024e0dd615b634f8e0020e2d41fe2b95
Naratip <naratip.18257@gmail.com>
2023-06-11 13:47:19 +0700

    make createGrid to push grid to DOM

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ function createGrid( size = 16){
     const grid = document.createElement("div");
     grid.style.cssText = `padding:${size};margin:0;border:0;`;
     grid.classList.add("paper");
+    container.appendChild(grid);
 }
